d3e1298acf37312db4cfc888dee7d7764cad1e6a
Arky <hitmanarky@gmail.com>
2014-05-22 01:28:21 +0300

    updated Config.xml

---
+++ platforms/ubuntu/native/build/CMakeFiles/coreplugins.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arky/Dev/Cordova/expat-flashcards/platforms/ubuntu/native/build/coreplugins_automoc.cpp" "/home/arky/Dev/Cordova/expat-flashcards/platforms/ubuntu/native/build/CMakeFiles/coreplugins.dir/coreplugins_automoc.cpp.o"
+  "/home/arky/Dev/Cordova/expat-flashcards/platforms/ubuntu/build/src/coreplugins.cpp" "/home/arky/Dev/Cordova/expat-flashcards/platforms/ubuntu/native/build/CMakeFiles/coreplugins.dir/src/coreplugins.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "QT_CONTACTS_LIB"
+  "QT_CORE_LIB"
+  "QT_DBUS_LIB"
+  "QT_FEEDBACK_LIB"
+  "QT_GUI_LIB"
+  "QT_LOCATION_LIB"
+  "QT_MULTIMEDIAWIDGETS_LIB"
+  "QT_MULTIMEDIA_LIB"
+  "QT_NETWORK_LIB"
+  "QT_QML_LIB"
+  "QT_QUICK_LIB"
+  "QT_SENSORS_LIB"
+  "QT_SYSTEMINFO_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/arky/Dev/Cordova/expat-flashcards/platforms/ubuntu/native/build/CMakeFiles/cordovaubuntuplugin.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/home/arky/Dev/Cordova/expat-flashcards/platforms/ubuntu/build"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/include/qt5"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  "/usr/include/qt5/QtLocation"
+  "/usr/include/qt5/QtSensors"
+  "/usr/include/qt5/QtFeedback"
+  "/usr/include/qt5/QtSystemInfo"
+  "/usr/include/qt5/QtDBus"
+  "/usr/include/qt5/QtNetwork"
+  "/usr/include/qt5/QtContacts"
+  "/usr/include/qt5/QtMultimedia"
+  "/usr/include/qt5/QtQuick"
+  "/usr/include/qt5/QtQml"
+  "/usr/include/qt5/QtMultimediaWidgets"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
